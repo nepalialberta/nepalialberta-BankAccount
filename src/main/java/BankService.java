e4bfe5d3f7de2985f6ac0a3628c1fdc480272ec3
nepalialberta <nepalialberta@gmail.com>
2023-02-11 04:19:32 +0000

    Done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,6 +23,8 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        balance = balance+amount;
+
 
     }
 
@@ -32,6 +34,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+    if(amount>balance){
+
+}   else{
+    balance = balance-amount;
+}
 
     }
 
@@ -40,6 +47,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
